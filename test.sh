2f1e5171f674cbb675fca2a60ce9522ce743e122
choas <18810719030@163.com>
2017-09-20 15:36:44 +0800

    YOLOv1

---
+++ test.sh
@@ -0,0 +1,1 @@
+./darknet yolo valid cfg/yolo_darknet19.cfg backup/yolo_darknet19_final.weights 2>&1 | tee test.log
---
+++ train.sh
@@ -0,0 +1,1 @@
+./darknet yolo train cfg/yolo_darknet19.cfg weights/darknet19_224.conv.23 2>&1 | tee train.log
